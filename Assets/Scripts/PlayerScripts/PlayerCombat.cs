2ce0976c213b73afc2d3f72f3f6f46fb672d8772
Dat1Doge <zhangjiaweidaniel@gmail.com>
2025-07-16 19:30:06 -0700

    Weapon Rotates properly

--- Assets/Scripts/PlayerScripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerScripts/PlayerCombat.cs
@@ -1,17 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCombat : MonoBehaviour
 {
+    private Transform weaponPivot;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        weaponPivot = transform.Find("WeaponPivot");
+    }
+
+    private void pointWeapon()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
+        float zAngle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        // Flip the weapon if facing left by changing localScale.x
+        Vector3 pivotScale = weaponPivot.localScale;
+        pivotScale.y = direction.x < 0 ? -Mathf.Abs(pivotScale.x) : Mathf.Abs(pivotScale.x);
+        weaponPivot.localScale = pivotScale;
+        weaponPivot.rotation = Quaternion.Euler(new Vector3(0, 0, zAngle));
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-
+        pointWeapon();
     }
 
     public void Attack()
